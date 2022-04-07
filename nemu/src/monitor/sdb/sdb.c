0e87bbcb823ed4ed2f2a3002c43fb86b5d5036ee
tracer-ics2021 <tracer@njuics.org>
2022-04-07 02:01:56 -0500

    >  compile 201220001    Static hostname: shikye          Icon name: computer-vm            Chassis: vm         Machine ID: e6556fd22823408b9f9bf1bbc1423639            Boot ID: f4974643c817442e9cb7aaa0caf96868     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-10-amd64       Architecture: x86-64  02:01:56 up  3:12,  1 user,  load average: 0.07, 0.14, 0.17

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,9 +89,9 @@ static int cmd_info(char *args, char* arg_after[20]) {
   {
     for(int i = 0; i < 32; i++)
       {
-        printf("x[%d] is %u\n", i , cpu.gpr[i]._32);
+        printf("x[%d]\t is %u\n", i , cpu.gpr[i]._32);
       }
-    printf("pc is %u\n",cpu.pc);
+    printf("pc\t\t is %u\n",cpu.pc);
   }
   else if(!strcmp(args,"w"))
   {
