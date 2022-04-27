0a08b9868ec1cd77221fd34f3c7052f01b07a365
tracer-ics2021 <tracer@njuics.org>
2022-04-26 21:47:01 -0500

    >  compile 201220001    Static hostname: shikye          Icon name: computer-vm            Chassis: vm         Machine ID: e6556fd22823408b9f9bf1bbc1423639            Boot ID: 5cc2ecb96b824519b2cf887230ce227a     Virtualization: vmware   Operating System: Debian GNU/Linux 11 (bullseye)             Kernel: Linux 5.10.0-10-amd64       Architecture: x86-64  21:47:01 up  1:02,  1 user,  load average: 0.18, 0.25, 0.18

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -4,14 +4,31 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
+  int arg_n = 1;
+  unsigned int comp1 = 0;
+  unsigned int comp2 = 0;
+  bool flag = 0;
+
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
 #else
   init_monitor(argc, argv);
 #endif
+
+  while(argv[arg_n] != NULL)
+  {
+    comp1 = atoi(argv[arg_n]);
+    arg_n ++;
+    comp2 = expr(argv[arg_n],&flag);
+
+    if(comp1 != comp2)
+      printf("error!");
+  }
+  
 
   /* Start engine. */
   engine_start();
